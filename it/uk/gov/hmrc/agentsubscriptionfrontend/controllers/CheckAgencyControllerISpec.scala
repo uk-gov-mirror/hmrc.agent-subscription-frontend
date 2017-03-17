2e75956003d49b19f2af2e19205f9e53b7f05b9c
Richard Gomes <richard.gomes@digital.hmrc.gov.uk>
2017-03-17 10:38:55 +0000

    [RG][APB-687] Add integration test for UTR which fails Modulus11Check validation.

--- it/uk/gov/hmrc/agentsubscriptionfrontend/controllers/CheckAgencyControllerISpec.scala
+++ it/uk/gov/hmrc/agentsubscriptionfrontend/controllers/CheckAgencyControllerISpec.scala
@@ -23,7 +23,6 @@ import uk.gov.hmrc.agentsubscriptionfrontend.support.SampleUsers._
 class CheckAgencyControllerISpec extends BaseControllerISpec with SessionDataMissingSpec {
 
   private val validUtr = "2000000000"
-  private val invalidUtr = "0123456"
   private val validPostcode = "AA1 1AA"
   private val invalidPostcode = "not a postcode"
 
@@ -69,6 +68,22 @@ class CheckAgencyControllerISpec extends BaseControllerISpec with SessionDataMis
     behave like anAgentAffinityGroupOnlyEndpoint(request => controller.checkAgencyStatus(request))
 
     "return a 200 response to redisplay the form with an error message for invalidly-formatted UTR" in {
+      val invalidUtr = "0123456"
+      AuthStub.hasNoEnrolments(subscribingAgent)
+      val request = authenticatedRequest()
+        .withFormUrlEncodedBody("utr" -> invalidUtr, "postcode" -> validPostcode)
+      val result = await(controller.checkAgencyStatus(request))
+
+      status(result) shouldBe OK
+      val responseBody = bodyOf(result)
+      responseBody should include("Check Agency Status")
+      responseBody should include ("Please enter a valid UTR")
+      responseBody should include (invalidUtr)
+      responseBody should include (validPostcode)
+    }
+
+    "return a 200 response to redisplay the form with an error message for UTR failing to pass Modulus11Check" in {
+      val invalidUtr = "2000000001" // Modulus11Check validation fails in this case
       AuthStub.hasNoEnrolments(subscribingAgent)
       val request = authenticatedRequest()
         .withFormUrlEncodedBody("utr" -> invalidUtr, "postcode" -> validPostcode)
