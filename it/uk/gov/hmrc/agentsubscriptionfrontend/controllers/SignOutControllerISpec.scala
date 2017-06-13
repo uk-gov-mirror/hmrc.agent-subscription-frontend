d3ffe5d1c6e7a691cd3156ea8a6aacdc2b2d08af
dimitra.karatza <dimitra.karatza@digital.hmrc.gov.uk>
2017-06-13 15:36:26 +0100

    [DK][APB-895] signed-out, fix integration tests respectively.

---
+++ it/uk/gov/hmrc/agentsubscriptionfrontend/controllers/SignOutControllerISpec.scala
@@ -0,0 +1,21 @@
+package uk.gov.hmrc.agentsubscriptionfrontend.controllers
+
+import play.api.test.FakeRequest
+import play.api.test.Helpers.redirectLocation
+import play.api.test.Helpers._
+
+class SignOutControllerISpec extends BaseControllerISpec {
+  private lazy val controller: SignedOutController = app.injector.instanceOf[SignedOutController]
+
+  private val fakeRequest = FakeRequest()
+
+  "context signed out" should {
+    "redirect to start page" in {
+      val result = await(controller.signOut(fakeRequest))
+
+      status(result) shouldBe 303
+      redirectLocation(result).head should include("/start")
+    }
+  }
+}
+
