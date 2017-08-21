fa08d640c479910ee5292b0e0199f8dce1b47d39
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2017-08-21 14:41:38 +0100

    [FD,TA] Make start.path follow URL path conventions
    The path component of a URL includes the leading slash - see for example https://developer.mozilla.org/en-US/docs/Learn/Common_questions/What_is_a_URL#Basics_anatomy_of_a_URL

--- app/uk/gov/hmrc/agentsubscriptionfrontend/config/frontendAppConfig.scala
+++ app/uk/gov/hmrc/agentsubscriptionfrontend/config/frontendAppConfig.scala
@@ -68,5 +68,5 @@ class FrontendAppConfig extends AppConfig with StrictConfig with ServicesConfig 
   override lazy val blacklistedPostcodes: Set[String] =
     PostcodesLoader.load("/po_box_postcodes_abp_49.csv").map(x => x.toUpperCase.replace(" ", "")).toSet
   override lazy val journeyName: String = getConfString("address-lookup-frontend.journeyName", "")
-  override lazy val agentServicesAccountUrl: String = s"$servicesAccountUrl/$servicesAccountPath"
+  override lazy val agentServicesAccountUrl: String = s"$servicesAccountUrl$servicesAccountPath"
 }
