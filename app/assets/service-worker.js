adbbe2b5d13c8f5cf3bd0b64c48290fa97c36f53
adamliversidge <9948188+adamliversidge@users.noreply.github.com>
2019-04-01 11:50:06 +0100

    APB-3504 Service worker for agents

--- app/assets/service-worker.js
+++ app/assets/service-worker.js
@@ -1,20 +1,42 @@
 // Installation: Store offline page
-self.addEventListener('install', function(event) {
-    var offlineSite = new Request('offline.html');
+self.addEventListener('install', function (event) {
+    console.log('WORKER: install event in progress.');
     event.waitUntil(
-        fetch(offlineSite).then(function(response) {
-            return caches.open('agents-offline').then(function(cache) {
-                console.log('[PWA Info] Cached offline page');
-                return cache.put(offlineSite, response);
-            });
-        }));
+        caches.open('agents-offline').then(function (cache) {
+            console.log('[PWA Info] Cached offline page');
+            return cache.addAll([
+                'manifest.json',
+                'offline.html',
+                '/template/assets/images/gov.uk_logotype_crown.png',
+                '/template/assets/images/open-government-licence_2x.png',
+                '/template/assets/stylesheets/images/govuk-crest.png',
+                '/template/assets/stylesheets/govuk-template.css',
+                '/template/assets/stylesheets/fonts.css',
+                '/template/assets/stylesheets/govuk-template-print.css',
+                '/agent-subscription/assets/stylesheets/styles.css',
+                '/agent-subscription/assets/stylesheets/offline.css',
+                '/agent-subscription/assets/javascripts/accessible-autocomplete.min.js',
+                '/agent-subscription/assets/javascripts/details.pollyfill.js',
+                '/agent-subscription/assets/javascripts/ga-events.js',
+                '/agent-subscription/assets/javascripts/scripts.js'
+                // 'http://localhost:9032/assets/3.0.2/images/crests/hmrc_crest_18px.png',
+                // 'http://localhost:9032/assets/3.0.2/images/icon-pointer.png',
+                // 'http://localhost:9032/assets/3.0.2/javascripts/vendor/modernizr.js',
+                // 'http://localhost:9032/assets/3.0.2/javascripts/application.min.js',
+                // 'http://localhost:9032/assets/3.0.2/stylesheets/application.min.css'
+            ]);
+        })
+            .then(function () {
+                console.log('WORKER: install completed');
+            })
+    );
 });
 
 // Serve offline page if the fetch fails
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         fetch(event.request).catch(function(error) {
-                console.error( '[PWA Info] App offline. Serving stored offline page: ' + error );
+                console.log( '[PWA Info] App offline. Serving stored offline page: ' + error );
                 return caches.open('agents-offline').then(function(cache) {
                     return cache.match('offline.html');
                 });
--- app/assets/sw.js
+++ app/assets/sw.js
@@ -1,11 +1,13 @@
-//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
-if (navigator.serviceWorker.controller) {
-    console.log('[PWA Info] active service worker found, no need to register')
-} else {
-    //Register the ServiceWorker
-    navigator.serviceWorker.register('/service-worker.js', {
-        scope: '/'
-    }).then(function (reg) {
-        console.log('Service worker has been registered for scope:' + reg.scope);
-    });
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
+if('serviceWorker' in navigator){
+    if (navigator.serviceWorker.controller) {
+        console.log('[PWA Info] active service worker found, no need to register')
+    } else {
+        // Register the ServiceWorker
+        navigator.serviceWorker.register('/service-worker.js', {
+            scope: '/'
+        }).then(function (reg) {
+            console.log('Service worker has been registered for scope:' + reg.scope);
+        });
+    }
 }
