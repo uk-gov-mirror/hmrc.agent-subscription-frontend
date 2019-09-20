fec6a42e90d4e600aa11d13c9579c5a64c87bd4f
Adam Liversidge <9948188+adamliversidge@users.noreply.github.com>
2019-09-20 15:36:19 +0100

    Apb 4245
    * APB-4245[AL] Accessibility statement - initial commit
    
    * APB-4245[AL] Accessibility statement
    
    * APB-4245[AL] Accessibility statement
    
    * APB-4245[AL] Update content
    
    * APB-4245[AL] Update accessibility statement content
    
    * APB-4245[AL] Update accessibility statement content
    
    * APB-4245[AL] Update accessibility statement content

---
+++ app/uk/gov/hmrc/agentsubscriptionfrontend/controllers/AccessibilityStatementController.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.agentsubscriptionfrontend.controllers
+
+import com.kenshoo.play.metrics.Metrics
+import javax.inject.{Inject, Singleton}
+import play.api.i18n.MessagesApi
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.agentsubscriptionfrontend.config.AppConfig
+import uk.gov.hmrc.agentsubscriptionfrontend.models.ContinueId
+import uk.gov.hmrc.agentsubscriptionfrontend.service.{SessionStoreService, SubscriptionJourneyService, SubscriptionService}
+import uk.gov.hmrc.agentsubscriptionfrontend.util.toFuture
+import uk.gov.hmrc.agentsubscriptionfrontend.views.html
+import uk.gov.hmrc.auth.core.AuthConnector
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class AccessibilityStatementController @Inject()(
+  override val authConnector: AuthConnector,
+  continueUrlActions: ContinueUrlActions,
+  val sessionStoreService: SessionStoreService,
+  subscriptionService: SubscriptionService,
+  subscriptionJourneyService: SubscriptionJourneyService)(
+  implicit override implicit val appConfig: AppConfig,
+  metrics: Metrics,
+  override val messagesApi: MessagesApi,
+  val ec: ExecutionContext)
+    extends AgentSubscriptionBaseController(authConnector, continueUrlActions, appConfig, subscriptionJourneyService)
+    with SessionBehaviour {
+
+  def showAccessibilityStatement: Action[AnyContent] = Action { implicit request =>
+    Ok(html.accessibility_statement())
+  }
+}
