6321e145e5b260185ae36164c9650e1b128e629d
Marianne Pearson <32272884+mariannepearson0@users.noreply.github.com>
2019-11-14 13:53:11 +0000

    changing url to path

--- app/uk/gov/hmrc/agentsubscriptionfrontend/config/frontendAppConfig.scala
+++ app/uk/gov/hmrc/agentsubscriptionfrontend/config/frontendAppConfig.scala
@@ -90,7 +90,7 @@ class FrontendAppConfig @Inject()(servicesConfig: ServicesConfig) extends AppCon
   override val sessionCacheBaseUrl: String = servicesConfig.baseUrl("cachable.session-cache")
 
   override val companyAuthSignInUrl: String =
-    s"${getConf("microservice.services.company-auth-frontend.external-url")}${getConf("microservice.services.company-auth-frontend.sign-in-url")}"
+    s"${getConf("microservice.services.company-auth-frontend.external-url")}${getConf("microservice.services.company-auth-frontend.sign-in-path")}"
   override val chainedSessionDetailsTtl: Int = servicesConfig.getInt("mongodb.chainedsessiondetails.ttl")
   override val cachableSessionDomain: String =
     getConf("microservice.services.cachable.session-cache.domain")
